bb72132b88acc90b4f61d49e76e0b9ff2956163d
Wilker <walkerxr@hotmail.com>
2018-02-24 15:53:03 -0300

    added a real "read-me", also changed some code, i think .-.

--- src/jarra/JarraEx1_3.java
+++ src/jarra/JarraEx1_3.java
@@ -2,8 +2,8 @@ package jarra;
 
 public class JarraEx1_3 {
 	public static void main(String args[]) {
-		int x=10;
-		int y=x;
+		int		x=10;
+		double	y=x;
 		System.out.print(y); //imprime y que é igual a x 
 	}
 }
---
+++ src/jarra/JarraEx2_1.java
@@ -0,0 +1,15 @@
+package jarra;
+
+public class JarraEx2_1 {
+	public static void main(String[] args) {
+		String str[]= { "Curso de Java", " - Exemplo String" };
+		
+		System.out.println("\t"+str[0]+str[1]);
+		System.out.println("\nNúmero de caracteres:\t"+(str[0].length()+str[1].length()));
+		
+		//fazer ex6,df1
+			//TODO ex6: "crie uma nova String, pegando o pedaço da primeira String que compreenda a frase \"Exemplo String\"."
+		
+			//TODO df1: "faça o mesmo do exercício 6, porém sem usar posição (índice) fixa."
+	}
+}
