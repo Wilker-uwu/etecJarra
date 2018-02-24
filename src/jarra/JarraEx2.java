79b2f47c4a36ec044ea20d4a1d486195643f2181
Wilker <walkerxr@hotmail.com>
2018-02-24 14:47:29 -0300

    changed file names

--- src/jarra/JarraEx2.java
+++ src/jarra/JarraEx2.java
@@ -2,9 +2,7 @@ package jarra;
 
 public class JarraEx2 {
 	public static void main(String[] args) {
-		String str[]={"Curso de Java ","- Exemplo String"};
+		String str= "Curso de Java";
 		
-		System.out.println(str[0]+str[1]);
-		System.out.println("qtd de caracteres:	"+(str[0].length()+str[1].length()));
 	}
 }
