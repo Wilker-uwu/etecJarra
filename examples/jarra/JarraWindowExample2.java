11f16c8b153e0d2ddbfaf74fc1e84d43bf739e05
WilkerS1 <walkerxr@hotmail.com>
2018-04-19 20:44:51 -0300

    TextArea, CheckBoxes and (TODO) RadioBoxes (example window 2)

---
+++ examples/jarra/JarraWindowExample2.java
@@ -0,0 +1,118 @@
+package jarra;
+
+import java.awt.Color;
+import java.awt.Container; //Container is responsible for the background and for enabling the layouts
+
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+
+@SuppressWarnings("serial")
+public class JarraWindowExample2 extends JFrame{
+	
+	JLabel			lblName 	= new JLabel("Nome: ");
+	JTextField		txtName		= new JTextField("lol");
+	
+	JButton			btn1		= new JButton("1"),
+					btn2		= new JButton("2"),
+					btn3		= new JButton("3");
+	
+	JTextArea		txtaText	= new JTextArea("PLACEHOLDER TEXT",16,32);
+		JScrollPane	scrText		= new JScrollPane(txtaText);
+	
+	ButtonGroup		grpOption	= null;
+	
+	JCheckBox[]		ckbOption	=	{
+									new JCheckBox("Option 0"),
+									new JCheckBox("Option 1"),
+									new JCheckBox("Option 2")
+									};
+	
+	@SuppressWarnings("unused")
+	public JarraWindowExample2() {
+		
+		super("Window name");
+		
+		Container pane = this.getContentPane();
+		pane.setLayout(null);
+		
+		
+		//sets up the grid
+		int gW		= 8,
+			gH		= 8,
+		
+			btnW	= gW*4,
+			btnH	= 26,
+			
+			wWidth	= btnW*3+gW*16,
+			wHeight = btnH*8+gH*11,
+			
+			ckbW	= wWidth-(gW*4),
+			ckbH	= 20;
+		
+		//sets up colors
+		Color	buttonsNumbers	= new Color(232,200,232),
+				buttonsMemory	= new Color(190,190,232),
+				buttonsFunctions= new Color(210,210,232),
+				bgColor			= new Color(186,255,223);
+		
+		txtaText.setBounds		(gW*1,			gH*1,					wWidth-(gW*4),	btnH*4);
+		
+		ckbOption[0].setBounds	(gW*1+btnW*0,	gH*2+btnH*4+ckbH*0,		ckbW,			ckbH);
+		ckbOption[1].setBounds	(gW*1+btnW*0,	gH*2+btnH*4+ckbH*1,		ckbW,			ckbH);
+		ckbOption[2].setBounds	(gW*1+btnW*0,	gH*2+btnH*4+ckbH*2,		ckbW,			ckbH);
+		
+		btn1.setBounds			(gW*2+btnW*1,	gH*3+btnH*4+ckbH*3,		btnW,			btnH);
+		btn2.setBounds			(gW*3+btnW*2,	gH*3+btnH*4+ckbH*3,		btnW,			btnH);
+		btn3.setBounds			(gW*4+btnW*3,	gH*3+btnH*4+ckbH*3,		btnW,			btnH);
+		
+		
+		txtaText.setBorder(null);
+		btn1.setBorder(null);
+		btn2.setBorder(null);
+		btn3.setBorder(null);
+		
+		btn1.setBackground(buttonsFunctions);
+		btn2.setBackground(buttonsFunctions);
+		btn3.setBackground(buttonsFunctions);
+		
+		
+		scrText.setHorizontalScrollBarPolicy(
+				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		scrText.setVerticalScrollBarPolicy(
+				JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		
+		
+		
+		pane.add(txtaText);
+		
+		//TODO complete
+		for (int i = 0; i < grpOption.length; i++){
+			ckbOption[i].setBorder(null);
+			grpOption.add(ckbOption[i]);
+			pane.add(ckbOption[i]);
+		}
+		pane.add(btn1);
+		pane.add(btn2);
+		pane.add(btn3);
+		
+		pane.setBackground(bgColor);
+		this.setVisible(true);
+		this.setSize(wWidth, wHeight);
+		this.setLocation(520, 325);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		
+		JarraWindowExample2 window = new JarraWindowExample2(); //calls the window for usage
+		
+	}
+
+}
--- exercises/view/PessoaFisica.java
+++ exercises/view/PessoaFisica.java
@@ -1,7 +1,5 @@
-//TODO fix this eh
-/*package view;
+package view;
 
-import java.awt.Color;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -19,96 +17,138 @@ import javax.swing.text.MaskFormatter;
 @SuppressWarnings("serial")
 public class PessoaFisica extends JFrame{
 	
-	PessoaFisica		pessoaFisica = new PessoaFisica();
+	JTextField txtNome = new JTextField();
+	JLabel lblNome = new JLabel("NOME: ");
 	
-	JTextField			txtNome		= new JTextField(),
-						txtEnd		= new JTextField(),
-						txtBairro	= new JTextField(),
-						txtCidade	= new JTextField();
+	JTextField txtEnd = new JTextField();
+	JLabel lblEnd = new JLabel("ENDEREÇO: ");
 	
-	JFormattedTextField	txtCep		= null,
-						txtTel		= null,
-						txtCel		= null,
-						txtRg		= null,
-						txtCpf		= null;
+	JTextField txtBairro = new JTextField();
+	JLabel bairro = new JLabel("BAIRRO: ");
 	
-	JLabel				lblNome		= new JLabel("NOME: "),
-						lblEnd		= new JLabel("ENDEREÇO: "),
-						lblBairro	= new JLabel("BAIRRO: "),
-						lblCep		= new JLabel("CEP: "),
-						lblCidade	= new JLabel("CIDADE: "),	
-						lblEstado	= new JLabel("ESTADO: "),
-						lblTel		= new JLabel("TELEFONE: "),
-						lblCel		= new JLabel("CELULAR: "),
-						lblRg		= new JLabel("RG: "),
-						lblCpf		= new JLabel("CPF: "),
-						lblSexo		= new JLabel("SEXO: ");
+	JFormattedTextField txtCep = null; //txt formatado
+	JLabel cep = new JLabel("CEP: ");
 	
-	JComboBox			cmbEst		= new JComboBox(); //opções de estados
+	JTextField txtCidade = new JTextField();
+	JLabel cidade = new JLabel("CIDADE: ");	
 	
-	JButton				btnSalvar	= new JButton("Salvar"),
-						btnImprimir	= new JButton("Imprimir");
+	JLabel est = new JLabel("ESTADO: ");
+	JComboBox cmbEst = new JComboBox(); //aparecer opcoes dos estados
 	
-	JRadioButton[]		rdbsexo		= new JRadioButton[2]; //botão para opçoes
-	ButtonGroup			grupo		= new ButtonGroup(); {  //group impede marcar mais de uma opçao no mesmo objeto
+	JFormattedTextField txtTel = null;
+	JLabel tel = new JLabel("TELEFONE: ");
 	
-		rdbsexo[0] = new JRadioButton("Feminino");
-		rdbsexo[1] = new JRadioButton("Masculino"); //fechar chave nos groups
-		
-		MaskFormatter		formatTel	= null, //add mascara
-							formatCel	= null,
-							formatCpf	= null,
-							formatCep	= null,
-							formatRg	= null;
-		}
+	JFormattedTextField txtCel = null;
+	JLabel cel = new JLabel("CELULAR: ");
 	
-	private String		infNome		= null,
-						infEndereco	= null;
+	JFormattedTextField txtRg = null;
+	JLabel rg = new JLabel("RG: ");
 	
-	public String getInfNome() {
-		return infNome;
+	JFormattedTextField txtCpf = null;
+	JLabel cpf = new JLabel("CPF: ");
+	
+	JButton btnSalvar = new JButton("Salvar");
+	
+	JButton btnImprimir = new JButton("Imprimir");
+	
+	JLabel sexo = new JLabel("SEXO: ");
+	
+	JRadioButton[] rdbsexo = new JRadioButton[2]; //botão para opçoes
+	ButtonGroup grupo = new ButtonGroup();{ //group impede marcar mais de uma opçao no mesmo objeto
+	
+	rdbsexo[0] = new JRadioButton("Feminino");
+	rdbsexo[1] = new JRadioButton("Masculino");} //fechar chave nos groups
+	
+	MaskFormatter formatTel = null; //add mascara
+	MaskFormatter formatCel = null;
+	MaskFormatter formatCpf = null;
+	MaskFormatter formatCep = null;
+	MaskFormatter formatRg = null;
+	
+	private static String nome=null;
+	private static String endereco=null;
+	
+	public static String getNome() {
+		return nome;
 	}
-	public void setInfNome(String infNome) {
-		this.infNome = infNome;
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
-	public String getInfEndereco() {
-		return infEndereco;
+	public static String getEndereco() {
+		return endereco;
 	}
-	public void setInfEndereco(String infEndereco) {
-		this.infEndereco = infEndereco;
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
 	}
 	
-	
+	PessoaFisica pessoaFisica = new PessoaFisica();
+
 	public PessoaFisica(){
 		super("Cadastro PF");
 		
-		Container paine = this.getContentPane(); paine.setLayout(null);
+		Container paine = this.getContentPane();
 		
+		paine.add(lblNome);
+		paine.add(txtNome);	
+		lblNome.setBounds(10, 15, 45, 30);
+		txtNome.setBounds(90, 15, 225, 30);
 		
-		//sets up the grid
-		int		gW		= 8,
-				gH		= 8,
-		
-				btnW	= gW*4,
-				btnH	= 26,
+		paine.add(lblEnd);
+		paine.add(txtEnd);	
+		lblEnd.setBounds(10, 50, 70, 30);
+		txtEnd.setBounds(90, 50, 225, 30);
 			
-				wWidth	= btnW*5+gW*7,
-				wHeight = btnH*8+gH*11;
-
-
 		try { //add mascara antes de tudo!!!
 			formatCep = new MaskFormatter("#####-###"); //a mascara
 			txtCep = new JFormattedTextField(formatCep);
 		} catch (Exception ex) { //exibir o erro
 			ex.printStackTrace();
 		}
+		paine.add(cep);
+		paine.add(txtCep);			
+		cep.setBounds(335, 70, 40, 30);
+		txtCep.setBounds(400, 70, 120, 30);
 		
+		
+		paine.add(cidade);
+		paine.add(txtCidade);	
+		cidade.setBounds(10, 85, 70, 30);
+		txtCidade.setBounds(90, 85, 225, 30);
+		
+		paine.add(est);
+		est.setBounds(335, 105, 70, 30);
+		paine.add(cmbEst);
+		cmbEst.setBounds(400, 105, 70, 30);
+		cmbEst.addItem("AC");		cmbEst.addItem("AL");
+		cmbEst.addItem("AP");		cmbEst.addItem("AM");
+		cmbEst.addItem("BA");		cmbEst.addItem("CE");
+		cmbEst.addItem("DF");		cmbEst.addItem("ES");
+		cmbEst.addItem("GO");		cmbEst.addItem("MA");
+		cmbEst.addItem("MT");		cmbEst.addItem("MS");
+		cmbEst.addItem("MG");		cmbEst.addItem("PA");
+		cmbEst.addItem("PB");		cmbEst.addItem("PR");
+		cmbEst.addItem("PE");		cmbEst.addItem("PI");
+		cmbEst.addItem("RJ");		cmbEst.addItem("RN");
+		cmbEst.addItem("RS");		cmbEst.addItem("RO");
+		cmbEst.addItem("RR");		cmbEst.addItem("SC");
+		cmbEst.addItem("SP");		cmbEst.addItem("SE");
+		cmbEst.addItem("TO");		
+		
+		paine.add(bairro);
+		paine.add(txtBairro);	
+		bairro.setBounds(10, 120, 70, 30);
+		txtBairro.setBounds(90, 120, 225, 30);
+				
 		try {
 			formatTel = new MaskFormatter("(##)####-####"); //a mascara
 			txtTel = new JFormattedTextField(formatTel);
 		} catch (Exception ext) { //exibir o erro
 			ext.printStackTrace();
 		}
+		paine.add(tel);
+		paine.add(txtTel);	
+		tel.setBounds(10, 155, 70, 30);
+		txtTel.setBounds(90, 155, 155, 30);		
 		
 		try {
 			formatCel = new MaskFormatter("(##)#####-####"); //a mascara
@@ -116,39 +156,61 @@ public class PessoaFisica extends JFrame{
 		} catch (Exception exc) { //exibir o erro
 			exc.printStackTrace();
 		}
-
+		paine.add(cel);
+		paine.add(txtCel);	
+		cel.setBounds(335, 155, 70, 30);
+		txtCel.setBounds(400, 155, 155, 30);
+		
+		
+		paine.add(sexo);			
+		sexo.setBounds(335, 15, 55, 30);		
+		grupo.add(rdbsexo[0]); //grupo para o sexo
+		grupo.add(rdbsexo[1]);
+		paine.add(rdbsexo[0]); //opção para o sexo
+		rdbsexo[0].setBounds(375, 15, 85, 30);
+		paine.add(rdbsexo[1]);
+		rdbsexo[1].setBounds(455, 15, 90, 30);
+				
 		try {
 			formatRg = new MaskFormatter("##.###.###-A"); //a mascara
 			txtRg = new JFormattedTextField(formatRg);
 		} catch (Exception exr) { //exibir o erro
 			exr.printStackTrace();
 		}
-
+		paine.add(rg);
+		paine.add(txtRg);	
+		rg.setBounds(10, 190, 55, 30);
+		txtRg.setBounds(90, 190, 155, 30);		
+		
 		try {
 			formatCpf = new MaskFormatter("###.###.###-##"); //a mascara
 			txtCpf = new JFormattedTextField(formatCpf);
 		} catch (Exception exf) { //exibir o erro
 			exf.printStackTrace();
 		}
-	
+		paine.add(cpf);
+		paine.add(txtCpf);	
+		cpf.setBounds(335, 190, 70, 30);
+		txtCpf.setBounds(400, 190, 155, 30);
 		
-
+		paine.add(btnSalvar);
+		btnSalvar.setBounds(250, 250, 130, 30);
 		btnSalvar.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
-				pessoaFisica.setInfNome(txtNome.getText());
-				pessoaFisica.setInfEndereco(txtEnd.getText());
+				setNome(txtNome.getText());
+				setEndereco(txtEnd.getText());
 			}
 		});
-
+		
+		paine.add(btnImprimir);
+		btnImprimir.setBounds(390, 250, 130, 30);
+		
 		this.setLayout(null);
 		this.setVisible(true);
-		this.setSize(wWidth, wHeight);
+		this.setSize(600, 330);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
-	
-
-	@SuppressWarnings("unused")
 	public static void main(String args[]){
-		
+		PessoaFisica cadpf = new PessoaFisica();
 	}
-}*/
+}
